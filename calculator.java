232c72092c0a9b097943268e2ab2b1f86f60a86f
Anushka Gupta <90717843+anushkagupta12345@users.noreply.github.com>
2023-07-06 01:08:01 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,50 +1,30 @@
-// Java program to create a simple calculator
-// with basic +, -, /, * using java swing elements
+
 
 import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 
 class calculator extends JFrame implements ActionListener {
-	// create a frame
+
 	static JFrame f;
-
-	// create a textfield
 	static JTextField l;
-
-	// store operator and operands
 	String s0, s1, s2;
-
-	// default constructor
 	calculator() {
 		s0 = s1 = s2 = "";
 	}
-
-	// main function
 	public static void main(String args[]) {
-		// create a frame
+	
 		f = new JFrame("calculator");
-
 		try {
-			// set look and feel
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
-
-		// create a object of class
-		calculator c = new calculator();
-
-		// create a textfield
+                calculator c = new calculator();
 		l = new JTextField(16);
-
-		// set the textfield to non editable
 		l.setEditable(false);
-
-		// create number buttons and some operators
 		JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, ba, bs, bd, bm, be, beq, beq1;
-
-		// create number buttons
+		
 		b0 = new JButton("0");
 		b1 = new JButton("1");
 		b2 = new JButton("2");
@@ -56,23 +36,16 @@ class calculator extends JFrame implements ActionListener {
 		b8 = new JButton("8");
 		b9 = new JButton("9");
 
-		// equals button
 		beq1 = new JButton("=");
-
-		// create operator buttons
 		ba = new JButton("+");
 		bs = new JButton("-");
 		bd = new JButton("/");
 		bm = new JButton("*");
 		beq = new JButton("C");
-
-		// create . button
 		be = new JButton(".");
 
-		// create a panel
 		JPanel p = new JPanel();
 
-		// add action listeners
 		bm.addActionListener(c);
 		bd.addActionListener(c);
 		bs.addActionListener(c);
@@ -91,7 +64,6 @@ class calculator extends JFrame implements ActionListener {
 		beq.addActionListener(c);
 		beq1.addActionListener(c);
 
-		// add elements to panel
 		p.add(l);
 		p.add(ba);
 		p.add(b1);
@@ -110,11 +82,9 @@ class calculator extends JFrame implements ActionListener {
 		p.add(b0);
 		p.add(beq);
 		p.add(beq1);
-
-		// set Background of panel
+		
 		p.setBackground(Color.blue);
 
-		// add panel to frame
 		f.add(p);
 
 		f.setSize(200, 220);
@@ -124,27 +94,23 @@ class calculator extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		String s = e.getActionCommand();
 
-		// if the value is a number
 		if ((s.charAt(0) >= '0' && s.charAt(0) <= '9') || s.charAt(0) == '.') {
-			// if operand is present then add to second no
+			
 			if (!s1.equals(""))
 				s2 = s2 + s;
 			else
 				s0 = s0 + s;
 
-			// set the value of text
 			l.setText(s0 + s1 + s2);
-		} else if (s.charAt(0) == 'C') {
-			// clear the one letter
+		} 
+		else if (s.charAt(0) == 'C') {
+
 			s0 = s1 = s2 = "";
+                        l.setText(s0 + s1 + s2);
+		} 
+		else if (s.charAt(0) == '=') {
 
-			// set the value of text
-			l.setText(s0 + s1 + s2);
-		} else if (s.charAt(0) == '=') {
-
-			double te;
-
-			// store the value in 1st
+                         double te;
 			if (s1.equals("+"))
 				te = (Double.parseDouble(s0) + Double.parseDouble(s2));
 			else if (s1.equals("-"))
@@ -153,8 +119,7 @@ class calculator extends JFrame implements ActionListener {
 				te = (Double.parseDouble(s0) / Double.parseDouble(s2));
 			else
 				te = (Double.parseDouble(s0) * Double.parseDouble(s2));
-
-			// set the value of text
+			
 			l.setText(s0 + s1 + s2 + "=" + te);
 
 			// convert it to string
@@ -162,14 +127,13 @@ class calculator extends JFrame implements ActionListener {
 
 			s1 = s2 = "";
 		} else {
-			// if there was no operand
+	
 			if (s1.equals("") || s2.equals(""))
 				s1 = s;
-			// else evaluate
+
 			else {
 				double te;
 
-				// store the value in 1st
 				if (s1.equals("+"))
 					te = (Double.parseDouble(s0) + Double.parseDouble(s2));
 				else if (s1.equals("-"))
@@ -179,17 +143,13 @@ class calculator extends JFrame implements ActionListener {
 				else
 					te = (Double.parseDouble(s0) * Double.parseDouble(s2));
 
-				// convert it to string
 				s0 = Double.toString(te);
 
-				// place the operator
 				s1 = s;
 
-				// make the operand blank
 				s2 = "";
 			}
 
-			// set the value of text
 			l.setText(s0 + s1 + s2);
 		}
 	}
